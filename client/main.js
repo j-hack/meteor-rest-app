23583c565ee6de0001696c4399396ec62415c63d
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-07-23 07:13:49 +0900

    STEP5.2: ブックマークリソース一覧定期取得(ポーリング)

--- client/main.js
+++ client/main.js
@@ -6,15 +6,21 @@ import './main.html';
 import Bookmarks from '../imports/api/bookmarks/bookmarks';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { HTTP } from 'meteor/http';
+import { Meteor } from 'meteor/meteor';
 
 Template.Bookmarks.onCreated(function() {
   this.bookmarks = new ReactiveVar();
 
-  // ブックマークリソース取得
-  HTTP.get('/api/bookmarks', (err, res) => {
-    if (err) { console.error(err); return; }
-    this.bookmarks.set(res.data.data);
-  });
+  const fetchBookmarks = () => {
+    // ブックマーク一覧取得
+    HTTP.get('/api/bookmarks', (err, res) => {
+      if (err) { console.error(err); return; }
+      this.bookmarks.set(res.data.data);
+    });
+  };
+  fetchBookmarks();
+
+  Meteor.setInterval(fetchBookmarks, 5000);
 });
 
 Template.Bookmarks.helpers({
