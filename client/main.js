c68d6059b8d78710c1001fef483ba6797b20fe92
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-07-22 23:30:09 +0900

    STEP3.5 Bookmarks コレクションの作成と表示

--- client/main.js
+++ client/main.js
@@ -3,28 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './main.html';
 
-Template.Bookmarks.helpers({
-  bookmarks: [
-    {
-      url: 'http://www.meteor.com/',
-      title: 'Meteor',
-    },
-    {
-      url: 'https://guide.meteor.com/',
-      title: 'Meteor Guide',
-    },
-    {
-      url: 'http://docs.meteor.com/',
-      title: 'Meteor API Docs',
-    },
-    {
-      url: 'https://forums.meteor.com/',
-      title: 'Meteor Forums',
-    },
-    {
-      url: 'https://crater.io/',
-      title: 'Crater.io',
-    },
-  ],
-});
+import Bookmarks from '../imports/api/bookmarks/bookmarks';
 
+Template.Bookmarks.helpers({
+  bookmarks: Bookmarks.find(),
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,39 @@
 import { Meteor } from 'meteor/meteor';
+import Bookmarks from '../imports/api/bookmarks/bookmarks';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // 初期データ投入
+  if (Bookmarks.find().count() === 0) {
+    const data = [
+      {
+        url: 'http://www.meteor.com/',
+        title: 'Meteor',
+        createdAt: new Date(),
+      },
+      {
+        url: 'https://guide.meteor.com/',
+        title: 'Meteor Guide',
+        createdAt: new Date(),
+      },
+      {
+        url: 'http://docs.meteor.com/',
+        title: 'Meteor API Docs',
+        createdAt: new Date(),
+      },
+      {
+        url: 'https://forums.meteor.com/',
+        title: 'Meteor Forums',
+        createdAt: new Date(),
+      },
+      {
+        url: 'https://crater.io/',
+        title: 'Crater.io',
+        createdAt: new Date(),
+      },
+    ];
+
+    data.forEach(bookmark => {
+      Bookmarks.insert(bookmark);
+    });
+  }
 });
